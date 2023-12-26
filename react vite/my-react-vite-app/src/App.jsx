1063e5e82d14f41b51c8e69f9f1f655dcd6aa506
Ruthtadesse <ruthtadesse4998@gmail.com>
2023-12-26 23:20:38 +0300

    updated the position and did week 2

--- react vite/my-react-vite-app/src/App.jsx
+++ react vite/my-react-vite-app/src/App.jsx
@@ -28,7 +28,8 @@ function App() {
   );
 }
 
-export default App;*/
+export default App;
+week 2
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -47,3 +48,40 @@ function App() {
 
 export default App;
 
+*/
+import React, { useState } from 'react';
+import Form from './Form';
+
+function App() {
+  const [show, setShow] = useState(true);
+  const [notes, setNotes] = useState([]);
+
+  const handleSaveNote = (newNote) => {
+    setNotes([...notes, newNote]);
+  };
+
+  return (
+    <div>
+      {show && <Form onSaveNote={handleSaveNote} />}
+      <button
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        {show ? 'Hide Form' : 'Show Form'}
+      </button>
+
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>
+            <h3>{note.main}</h3>
+            <p>{note.sub}</p>
+            <p>{note.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
